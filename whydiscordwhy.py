f122f509372f407ddd270cc48de1be7318888c55
Francesco Milano <milano.francesco@protonmail.com>
2025-01-06 00:42:02 +0100

    Update whydiscordwhy.py

--- whydiscordwhy.py
+++ whydiscordwhy.py
@@ -7,7 +7,7 @@ from os import path, remove
 from psutil import process_iter
 from sys import argv
 
-customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
 app = customtkinter.CTk()
