26578d431748af0fdcb56ddf91b500615884db73
framilano <milano.francesco@protonmail.com>
2026-04-09 02:37:08 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pyinstaller --noconsole whydiscordwhy.py --icon=icon.png --noconfirm
-cp -r venv/lib/python3.13/site-packages/tkinterdnd2 dist/whydiscordwhy/_internal/tkinterdnd2
+cp -r venv/lib/python3.14/site-packages/tkinterdnd2 dist/whydiscordwhy/_internal/tkinterdnd2
 cp icon.png dist/whydiscordwhy/icon.png
 cp cpu-theme.json dist/whydiscordwhy/cpu-theme.json
 cp amd-theme.json dist/whydiscordwhy/amd-theme.json
